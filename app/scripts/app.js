963816d2d2577dd679628aecb5fd45fa267bb3ee
momoiroshikibu <momoiroshikibu@gmail.com>
2014-05-01 23:01:20 +0900

    ラジオボタンの動的追加処理を実装

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,26 @@
 /*global define */
-define([], function () {
+define(['jquery'], function ($) {
     'use strict';
 
-    return '\'Allo \'Allo!';
+    // "1件追加ボタンの押下イベント
+    $('#add_color_button').on('click', function() {
+        $('#radio_wrapper_color').append('<label><input type="radio" name="color">Pink</input></label>');
+    });
+
+
+    // 全件追加ボタンの押下イベント
+    $('#add_animal_button').on('click', function() {
+
+        var $radioWrapperAnimal = $('#radio_wrapper_animal');
+
+        // 既にinputが存在する場合は削除
+        $radioWrapperAnimal.empty();
+
+        // 動物のradio項目を追加
+        var animals = ['Dog', 'Cat', 'Bird'];
+        animals.forEach(function(animal) {
+            $radioWrapperAnimal.append('<label><input type="radio" name="animal">' + animal + '</input></label>');
+        });
+    });
+
 });
